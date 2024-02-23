e7281c43f569ea875e1b010720c3c95c7f89fd3b
Sulaimon Taofik <Sulaimontaofik08@gmail.com>
2024-02-23 17:28:19 +0100

    merged chnages from bola

--- src/views/Dashboard/parentdashboard/ParentDashboard.jsx
+++ src/views/Dashboard/parentdashboard/ParentDashboard.jsx
@@ -4,13 +4,14 @@ import Tutor_SideBar from '../../reusableComponents/Tutor_SideBar';
 const ParentDashboard = () => {
   return (
     <>
+      <div>
       <Tutor_SideBar />
       <div>
       </div>
       <div className="parent-main-content">
         <MainParentDashboard />
       </div>
-      </div>
+    </div>
   )
 }
 
