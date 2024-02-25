4410fc81e03653749bc9c98d0aa66ff4c8e2aa0a
Abdulmalik Alayande <alaabdulmalik03@gmail.com>
2024-02-25 23:36:43 +0100

    comms

--- src/Components/dashboard/tutordashboard/pages/TutorDashboard.jsx
+++ src/Components/dashboard/tutordashboard/pages/TutorDashboard.jsx
@@ -6,30 +6,31 @@ import DateRangeCalendarCalendarsProp from "../components/Calendar";
 
 function TutorDashboard() {
   return (
-    <div>
       <div className="student-dashboard" style={{display: 'flex'}}>
-        <div className="ml-7 mt-8">
-          <SideBar />
-        </div>
-        <div className="dashboard-display-section" style={{ background-color: 'rgb(238, 237, 255)',
-  width: '96vw'}}>
-          <div className="ml-7 mt-5">
-            <h2 className="welcome" style={{
-  font-size: '30px',
-  font-family: 'Georgia',
-  color:#1a1a1a,
-  margin: 2% 4%,
-  text-align: center,
-  /* text-align: center; */
-}}>Welcome Back</h2>
+          <div className="ml-7 mt-8">
+              <SideBar />
           </div>
-          <Profile />
-          {/* <Student /> */}
+          <div className="ml-7 mt-5">
+                   <h2 className="welcome" style={{
+                        fontSize: '30px',
+                        fontFamily: 'Georgia',
+                        color:'#1a1a1a',
+                        margin: '2% 4%',
+                        textAlign: 'center',
+                    /* text-align: center; */
+                  }}>Welcome Back</h2>
+            </div>
+            <div>
+            
+           <div className="dashboard-display-section" style={{
+                backgroundColor: 'rgb(238, 237, 255)',width: '96vw'
+            }}>
+                <Profile />
+            </div>
+          <DateRangeCalendarCalendarsProp />
         </div>
-        <DateRangeCalendarCalendarsProp />
       </div>
-    </div>
-  );
+);
 }
 
 export default TutorDashboard;
