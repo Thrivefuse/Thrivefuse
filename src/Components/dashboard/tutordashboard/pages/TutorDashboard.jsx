a6961e634bfa3a6fefc4803a7f75e0ea57635f55
Sulaimon Taofik <Sulaimontaofik08@gmail.com>
2024-02-27 23:28:06 +0100

    tESTING

---
+++ src/Components/dashboard/tutordashboard/pages/TutorDashboard.jsx
@@ -0,0 +1,26 @@
+import React from "react";
+import SideBar from "../components/SideBar";
+import Profile from "./Profile";
+import DateRangeCalendarCalendarsProp from "../components/Calendar";
+
+function TutorDashboard() {
+  return (
+    <div>
+    <div className="student-dashboard">
+      <div className="ml-7 mt-8">
+        <SideBar />
+      </div>
+      <div className="dashboard-display-section">
+        <div className="ml-7 mt-5">
+          <h2 className="welcome">Welcome Back</h2>
+        </div>
+        <Profile />
+        {/* <Student /> */}
+      </div>
+      <DateRangeCalendarCalendarsProp />
+    </div>
+  </div>
+);
+}
+
+export default TutorDashboard;
