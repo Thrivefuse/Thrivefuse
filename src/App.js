f70f2b63421d56c17eaf9676c95b3c2f78370a1e
Abdulmalik Alayande <alaabdulmalik03@gmail.com>
2024-03-26 11:14:10 +0100

    Retrying Heroku or Vercel Deployment

--- src/App.js
+++ src/App.js
@@ -2,10 +2,15 @@ import React from "react";
 import LandingPage from "./views/landingpage/landingPage";
 import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
 import NotFoundPage from "./views/reusableComponents/NotFoundPage";
-import {HOME_PAGE, PARENT_DASHBOARD, PARENT_REGISTRATION_PAGE, TUTOR_REGISTRATION_PAGE} from "./utils/constants";
+import {
+    HOME_PAGE,
+    PARENT_REGISTRATION_PAGE,
+    TUTOR_DASHBOARD,
+    TUTOR_REGISTRATION_PAGE
+} from "./utils/constants";
 import TutorSignUp from "./views/auth/signup/tutorSignUp";
-import ParentDashboard from "./views/dashboard/parent/ParentDashboard";
 import ParentSignUp from "./views/auth/signup/parentSignUp";
+import TutorDashboard from "./views/dashboard/tutor/pages/TutorDashboard";
 
 function App() {
     return(
@@ -16,7 +21,7 @@ function App() {
                 <Route path="*" element={<NotFoundPage/>} />
                 <Route path={PARENT_REGISTRATION_PAGE} element={<ParentSignUp/>} />
                 <Route path={TUTOR_REGISTRATION_PAGE} element={<TutorSignUp/>} />
-                <Route path={PARENT_DASHBOARD} element={<ParentDashboard/>} />
+                <Route path={TUTOR_DASHBOARD} element={<TutorDashboard/>} />
             </Routes>
         </Router>
     );
