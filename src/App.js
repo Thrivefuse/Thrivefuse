40d7f80b609f0e8e5f416a1c02f70099a8f6f341
Abdulmalik Alayande <alaabdulmalik03@gmail.com>
2024-03-26 06:13:00 +0100

    Retrying Heroku or Vercel Deployment

--- src/App.js
+++ src/App.js
@@ -2,14 +2,7 @@ import React from "react";
 import LandingPage from "./views/landingpage/landingPage";
 import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
 import NotFoundPage from "./views/reusableComponents/NotFoundPage";
-import ParentSignUp from './views/auth/signup/parentSignUp'
-import {
-    TUTOR_REGISTRATION_PAGE, HOME_PAGE, PARENT_REGISTRATION_PAGE,
-    TUTOR_DASHBOARD, STUDENT_DASHBOARD, PARENT_DASHBOARD
-} from "./utils/constants";
-import TutorSignUp from "./views/auth/signup/tutorSignUp";
-import TutorDashboard from "./views/dashboard/tutordashboard/pages/TutorDashboard";
-import Student from "./views/dashboard/tutordashboard/pages/Student";
+import {HOME_PAGE} from "./utils/constants";
 
 function App() {
     return(
@@ -17,11 +10,7 @@ function App() {
             <Routes>
                 <Route path={"/"} element={<LandingPage/>} />
                 <Route path={HOME_PAGE} element={<LandingPage/>} />
-                <Route path={PARENT_REGISTRATION_PAGE} element={<ParentSignUp/>} />
-                <Route path={TUTOR_REGISTRATION_PAGE} element={<TutorSignUp/>} />
-                <Route path={TUTOR_DASHBOARD} element={<TutorDashboard />} />
-                <Route path={STUDENT_DASHBOARD} element={<Student/>} /> 
-              <Route path="*" element={<NotFoundPage/>} />
+                <Route path="*" element={<NotFoundPage/>} />
           </Routes>
       </Router> 
       );
