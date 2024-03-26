8844a12efeb1f86c7b3066b3105998351aa3a848
Abdulmalik Alayande <alaabdulmalik03@gmail.com>
2024-03-26 12:10:28 +0100

    Fixed The Big

--- src/App.js
+++ src/App.js
@@ -3,13 +3,16 @@ import LandingPage from "./views/landingpage/landingPage";
 import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
 import NotFoundPage from "./views/reusableComponents/NotFoundPage";
 import {
-    HOME_PAGE,
-    PARENT_REGISTRATION_PAGE, STUDENT_DASHBOARD,
+    HOME_PAGE, PARENT_DASHBOARD,
+    PARENT_REGISTRATION_PAGE, STUDENT_DASHBOARD, STUDENT_REGISTRATION_PAGE, TUTOR_DASHBOARD,
     TUTOR_REGISTRATION_PAGE
 } from "./utils/constants";
 import TutorSignUp from "./views/auth/signup/tutorSignUp";
 import ParentSignUp from "./views/auth/signup/parentSignUp";
 import StudentDashboard from "./views/dashboard/student/StudentDashboard";
+import ParentDashboard from "./views/dashboard/parent/ParentDashboard";
+import TutorDashboard from "./views/dashboard/tutor/pages/TutorDashboard";
+import StudentSignUp from "./views/auth/signup/studentSignUp";
 
 function App() {
     return(
@@ -20,7 +23,10 @@ function App() {
                 <Route path="*" element={<NotFoundPage/>} />
                 <Route path={PARENT_REGISTRATION_PAGE} element={<ParentSignUp/>} />
                 <Route path={TUTOR_REGISTRATION_PAGE} element={<TutorSignUp/>} />
+                <Route path={STUDENT_REGISTRATION_PAGE} element={<StudentSignUp/>} />
                 <Route path={STUDENT_DASHBOARD} element={<StudentDashboard/>} />
+                <Route path={PARENT_DASHBOARD} element={<ParentDashboard/>} />
+                <Route path={TUTOR_DASHBOARD} element={<TutorDashboard/>} />
             </Routes>
         </Router>
     );
